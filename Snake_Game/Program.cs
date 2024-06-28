a7d07ffe35dc24c4362ef0aba11df92d3c19102d
Lunatik4872 <gael.pailly@gmail.com>
2024-06-28 19:46:59 +0200

    init

---
+++ Snake_Game/Program.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_CsLo;
+
+public static class Program
+{
+    public static Task Main(string[] args)
+    {
+        Raylib.InitWindow(800, 600, "Snake_Game");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Raylib.RAYWHITE);
+            Raylib.DrawText("C'est un beau test peut etre !", 10, 10, 30, Raylib.VIOLET);
+            Raylib.EndDrawing();
+
+        }
+
+        Raylib.CloseWindow();
+
+        return Task.CompletedTask;
+    }
+}
